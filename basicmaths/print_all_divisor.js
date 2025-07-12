4f2d047ba2e39623f152ea5db323e859ad1796b2
Piyush Singh <piyusshthakur5@gmail.com>
2025-07-12 06:59:02 +0530

    print all divisor

---
+++ basicmaths/print_all_divisor.js
@@ -0,0 +1,20 @@
+// const printAllDivisors = (num) => {
+//   for (let i = 1; i <= num; i++) {
+//     if (num % i == 0) {
+//       console.log(i);
+//     }
+//   }
+// };
+
+const printAllDivisors = (num) => {
+  for (let i = 1; i <= Math.sqrt(num); i++) {
+    if (num % i == 0) {
+      console.log(i);
+      if (num / i != i) {
+        console.log(num / i);
+      }
+    }
+  }
+};
+
+printAllDivisors(36);
