38baf87dcbb6d9d95586aba701f420593ecbff8c
PiyushThakur1 <piyusshthakur5@gmail.com>
2025-11-23 13:32:09 +0530

    vector

--- basicmaths/count-digits.js
+++
@@ -1,9 +0,0 @@
-let digit = 4537676534;
-
-let count = 0;
-while (digit > 0) {
-  digit = Math.floor(digit / 10);
-  count++;
-}
-
-console.log(`Number of digits is ${count}`);
--- basicmaths/reverse-number.js
+++
@@ -1,9 +0,0 @@
-let num = 6578;
-let lastDigit;
-let reverseNum = 0;
-while (num > 0) {
-  lastDigit = num % 10;
-  num = Math.floor(num / 10);
-  reverseNum = reverseNum * 10 + lastDigit;
-}
-console.log(reverseNum);
