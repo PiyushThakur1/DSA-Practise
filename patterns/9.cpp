cd1bc384663ba1579099c8010184ec83372fdb0b
PiyushThakur1 <piyusshthakur5@gmail.com>
2025-12-30 00:47:13 +0530

    pattern qst

--- patterns/9.cpp
+++ patterns/9.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j <= i; j++)
+    for (int i = 1; i <= 2*n-1; i++){
+        int stars = i;
+        if(i > n) stars = 2*n-i;
+        for (int j = 1; j <= stars; j++)
         {
             cout<<"*";
         }
